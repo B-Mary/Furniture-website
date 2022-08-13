dd8f8c33cdde9dfc25bf547018ce7ca8a6590028
B-Mary <mari-bahlina@ukr.net>
2022-08-13 18:42:51 +0300

    latest fixes

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,17 @@
      $('.rightside-menu').addClass('rightside-menu--close')
     });
 
+
+    $('.header__btn-menu').on('click', function () {
+        $('.menu').toggleClass('menu--open')
+    });
+
+    if($(window).width() < 651){
+        $('.works-path__item--measurements').appendTo($('.works-path__items-box'))
+    }
+   
+ 
+
     $('.top__slider').slick({
         dots: true,
         arrows:false,
@@ -20,6 +31,64 @@
         slidesToScroll:10,
         dots: true,
         arrows:false,
+        responsive: [
+            {
+                breakpoint: 1700,
+                settings: {
+                    slidesToShow:8,
+                    slidesToScroll:8,
+                }
+            },
+            {
+                breakpoint:1511,
+                settings: {
+                    slidesToShow:6,
+                    slidesToScroll:6,
+                }
+            },
+            {
+                breakpoint:1000,
+                settings: {
+                    slidesToShow:5,
+                    slidesToScroll:5,
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow:4,
+                    slidesToScroll:4,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow:4,
+                    slidesToScroll:4,
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow:3,
+                    slidesToScroll:3,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow:2,
+                    slidesToScroll:2,
+                }
+            },
+            {
+                breakpoint: 370,
+                settings: {
+                    slidesToShow:2,
+                    slidesToScroll:2,
+                }
+            }
+        ]
     });
 
     $('.article-slider__box').slick({
